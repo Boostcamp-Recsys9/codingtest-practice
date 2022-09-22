59b16629ad953da06ffe0ad782ba10fa3edf3df1
Yujin Kim <sandwich17@ewhain.net>
2022-09-22 12:52:17 +0900

    updated programmers 게임 맵 최단거리

--- week1/yujin.py
+++ week1/yujin.py
@@ -1,27 +1,24 @@
-# programmers 게임 맵 최단거리
-# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+from collections import deque
 
-answer = []
-
-def dfs(maps,location,visit,m,n,cnt) :
-    x = location[0]
-    y = location[1]
-    if x<0 or x>=n or y<0 or y>=m : return
-    if x==n-1 and y==m-1 : 
-        answer.append(cnt)
-        return
-    if (not visit[x][y]) and (maps[x][y] == 1):
-        visit[x][y] = True
-        dfs(maps,(x+1,y),visit,m,n,cnt+1)
-        dfs(maps,(x-1,y),visit,m,n,cnt+1)
-        dfs(maps,(x,y+1),visit,m,n,cnt+1)
-        dfs(maps,(x,y-1),visit,m,n,cnt+1)
+dx=[0,0,-1,1]
+dy=[1,-1,0,0]
+    
+def bfs(n,m,maps):
+    queue = deque()
+    queue.append((0,0))
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if not (nx<0 or nx>=n or ny<0 or ny>=m) : 
+                if maps[nx][ny]==1 :
+                    maps[nx][ny]=maps[x][y]+1
+                    queue.append((nx,ny))
+    return maps[-1][-1]
 
 def solution(maps):
-    location = (0,0)
-    n = len(maps)
-    m = len(maps[0])
-    visit = [[False]*m for _ in range(n)]
-    dfs(maps,location,visit,m,n,1)
-    if len(answer) == 0 : return -1
-    return min(answer)
+    n, m = len(maps), len(maps[0])
+    answer = bfs(n,m,maps)
+    if answer == 1 : return -1
+    return answer
