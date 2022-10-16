74cb232029ef8c5521e769727d6538b4a2df2fca
JWS <dnjstka0307@gmail.com>
2022-10-16 16:23:19 +0900

    코테

--- week5/원삼/거리두기확인_프로그래머스.py
+++ week5/원삼/거리두기확인_프로그래머스.py
@@ -1,38 +1,31 @@
 from itertools import combinations
 def solution(places):
-    # 답 리스트
+    # 각 사람이 앉은 경우를 확인해서 각각 자리의 combination사용
     ans = []
-    # 총 다섯개 시험장
-    for z in range(5):
-        # 사람이 앉은 상황에 대한 combination 리스트
+    for z in range(5):      
         person_list = []
-        # try
         count = 0
-        # 각각 사람이 앉은 경우 확인 후 각 경우에 대해 좌표값을 combination
         for y in range(5):
             for x in range(5):
                 if places[z][x][y] == "P":
                     person_list.append([x,y])
 
-        # 각 좌표값에 대해 거리 구하기 ~ 1이하면 count +1 , 2일 경우 조건에 따라 지정, 이상이면 pass
+        # 각자리간 거리 확인 후 거리두기가 안지켜지면 count + 1
         for i in combinations(person_list,2):
             x_len = abs(i[0][0]-i[1][0])
             y_len = abs(i[0][1]-i[1][1])
+            # 각 자리 좌표의 최소 값 사용
             if x_len + y_len <= 1:
                 count += 1
-            # 각 두 좌표값의 최소값을 이용해서 파티션이 없는 경우 확인
             elif x_len + y_len == 2:
-                # P-P 모양으로 앉앗을 경우
                 if x_len == 0:
                     m_ = min(i[0][1], i[1][1])
                     if places[z][i[0][0]][m_+1] == "O":
                         count += 1
-                # | 모양으로  앉앗을 경우
                 elif y_len == 0:
                     m_ = min(i[0][0], i[1][0])
                     if places[z][m_+1][i[0][1]] == "O":
-                        count += 1            
-                # 2*2로 앉앗을 경우        
+                        count += 1                    
                 else:
                     m_1 = min(i[0][0], i[1][0])
                     m_2 = min(i[0][1], i[1][1])
@@ -40,17 +33,17 @@ def solution(places):
                         count += 1
             else:
                 pass
-            # 하나의 앉은 경우에 대해 하나라도안지켯으면 break후 0전달
+
+            # 한자리라도 count되었으면 0 넣기
             if count > 0:
                 ans.append(0)
                 break
             else:
                 pass
-        # range z 내에서 한번도 0이 나올건이 없엇다면 1 
+        # 최종적으로 모든 자리가 거리두기 count가 안되었다면 1 넣기
         if count == 0:
             ans.append(1)
                         
     return ans                   
                 
-                    
-                    
+                 
